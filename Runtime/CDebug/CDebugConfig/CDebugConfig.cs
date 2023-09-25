ccc3d63a458ed63df167a9a0f54f8eff3fac084a
廖小白 <goatman914@gmail.com>
2023-09-25 15:15:12 +0800

    publish 0.1.8

--- Runtime/CDebug/CDebugConfig/CDebugConfig.cs
+++ Runtime/CDebug/CDebugConfig/CDebugConfig.cs
@@ -7,7 +7,7 @@ namespace ClassifiedConsole.Runtime
 {
     internal class CDebugConfig
     {
-        public const string version = "0.1.7";
+        public const string version = "0.1.8";
 
         public static bool ArchiveOnPlay
         {
