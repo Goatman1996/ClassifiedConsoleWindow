82f5b454c6fe4d08cd7a20f6d3b1a9f6b0c5eb98
廖小白 <liaoxiaobai@liaoxiaobaideMacBook-Pro.local>
2024-04-11 12:36:50 +0800

    publish 1.0.1

--- Runtime/CDebug/CDebugConfig/CDebugConfig.cs
+++ Runtime/CDebug/CDebugConfig/CDebugConfig.cs
@@ -7,7 +7,7 @@ namespace ClassifiedConsole.Runtime
 {
     internal class CDebugConfig
     {
-        public const string version = "1.0.0";
+        public const string version = "1.0.1";
 
         public static bool ArchiveOnPlay
         {
